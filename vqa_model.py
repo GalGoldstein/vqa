1e62e9c7985aaab40a5ac7725e57b8bddcbaaab6
Yotam Martin <yotamm@breezometer.com>
2020-12-22 22:40:11 +0200

    updates

--- vqa_model.py
+++ vqa_model.py
@@ -249,7 +249,7 @@ if __name__ == '__main__':
         val_questions_json_path = 'data/v2_OpenEnded_mscoco_val2014_questions.json'
         label2ans_path_ = 'data/cache/train_label2ans.pkl'
 
-    batch_size = 64 if running_on_linux else 16
+    batch_size = 128 if running_on_linux else 16
     num_workers = 12 if running_on_linux else 0
     train_dataloader = DataLoader(vqa_train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
                                   collate_fn=lambda x: x, drop_last=False)
